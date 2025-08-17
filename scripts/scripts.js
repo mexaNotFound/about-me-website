e459eb92c48a47e25aa631e5ab664659093f41d8
luka <mexaturashvili@gmail.com>
2025-08-18 00:10:08 +0400

    first commit

---
+++ scripts/scripts.js
@@ -0,0 +1,78 @@
+let mainbox = document.getElementById("mainbox")
+let interests = document.getElementById("interests")
+let likesDislikes = document.getElementById("likes-dislikes")
+
+let audio = document.getElementById("his-theme")
+// let socials = document.getElementById("socials")
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+
+async function openinterests() {
+    mainbox.classList.add("hidden")
+    await sleep(500)
+    interests.classList.add("active")
+}
+
+async function openlikesdislikes() {
+    mainbox.classList.add("hidden")
+    await sleep(500)
+    likesDislikes.classList.add("active")
+}
+
+// async function opensocials() {
+//     mainbox.classList.add("hidden")
+//     await sleep(500)
+//     socials.classList.add("active")
+// }
+
+
+
+async function return_to_main_box() {
+    interests.classList.remove("active")
+    likesDislikes.classList.remove("active")
+    // socials.classList.remove("active")
+    await sleep(500)
+    mainbox.classList.remove("hidden")
+}
+
+async function typewriter_title(title) {
+    title = title.replaceAll(" ", "\u00A0")
+    document.title = title[0]
+    for (let index = 1; index < title.length; index++) {
+        await sleep(400)
+        let char = title[index];
+        document.title += char
+        
+    }
+}
+
+async function typewriter_clear() {
+    for (let index = document.title.length; index > 1; index--) {        
+        await sleep(400)
+        document.title = document.title.slice(0, -1)
+    }
+}
+
+
+async function typewriter_animate(list){
+    while (true) {
+        for (let index = 0; index < list.length; index++) {
+            let text = list[index];
+            await typewriter_title(text)
+            await sleep(1000)
+            await typewriter_clear()
+            await sleep(1000)
+            
+        }
+    }
+}
+
+
+function remove_bigclikclikbox(ts) {
+    audio.play();
+    typewriter_animate(["about me! ^.^", "@velocitymeow"])
+    ts.classList.add("hidden");
+}
